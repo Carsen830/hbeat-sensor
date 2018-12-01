6abb2e2367a529f2187abf181c1dd189adea591f
Carsen830 <39925614+Carsen830@users.noreply.github.com>
2018-12-01 15:40:17 -0500

    Update start.py

--- start.py
+++ start.py
@@ -1,15 +1,21 @@
 from pulsesensor import Pulsesensor
 import time
+import requests
 
 p = Pulsesensor()
 p.startAsyncBPM()
+
+ip = 'yourIPhere'
+address = 'http://' + ip + ':8080/pi'
 
 try:
     while True:
         bpm = p.BPM
         if bpm > 0:
+            r = requests.post(address, data = {'heartbeat': bpm})
             print("BPM: %d" % bpm)
         else:
+            r = requests.post(address, data = {'heartbeat': 0})
             print("No Heartbeat found")
         time.sleep(1)
 except:
